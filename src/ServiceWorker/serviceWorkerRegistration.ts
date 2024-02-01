7a9c9e5b88341668aaac561a6090605ab556b112
eivindborstad <eivind.borstad@kongsbergdigital.com>
2024-02-01 08:56:37 +0100

    Use stricter ts rules

--- src/ServiceWorker/serviceWorkerRegistration.ts
+++ src/ServiceWorker/serviceWorkerRegistration.ts
@@ -24,8 +24,10 @@ type Config = {
 };
 
 export function register(config?: Config) {
+  //@ts-ignore
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
+    //@ts-ignore
     const publicUrl = new URL(process.env.PUBLIC_URL ?? '', window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
@@ -34,6 +36,7 @@ export function register(config?: Config) {
       return;
     }
 
+    //@ts-ignore
     const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
     if (isLocalhost) {
